e4a1a5cce0f88937235653adaeecaf906ea15786
sanchai1997 <sanyakorn.Chai@gmail.com>
2023-02-25 17:03:07 +0700

    Update

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -17,8 +17,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
   <link href="<?php if($title != 'SandBox Education'){ echo '../'; } ?>assets/img/apple-touch-icon.png" rel="apple-touch-icon">
 
   <!-- Google Fonts -->
-  <link href="https://fonts.gstatic.com" rel="preconnect">
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+  <link href='https://fonts.googleapis.com/css?family=Sarabun' rel='stylesheet'>
 
   <!-- Vendor CSS Files -->
   <link href="<?php if($title != 'SandBox Education'){ echo '../'; } ?>assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
