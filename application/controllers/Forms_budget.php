0104bfd3e7eb7f610d6bef9d9cbb9218a3e61221
Kongkiat Khemkhao <k.khemkhao@gmail.com>
2023-02-26 08:45:33 +0700

    Create Forms_budget.php

---
+++ application/controllers/Forms_budget.php
@@ -0,0 +1,28 @@
+<?php defined('BASEPATH') or exit('No direct script access allowed');
+
+class Forms_budget extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->load->helper(array('form', 'url'));
+        $this->load->library('form-validation');
+    }
+
+    public function index()
+    {
+        if (!file_exists(APPPATH . 'views/pages/forms/forms-budget.php')) {
+            // Whoops, we don't have a page for that!
+            show_404();
+        }
+
+        $data['title'] = 'Forms School'; // Capitalize the first letter
+
+        $this->load->view('templates/header', $data);
+        $this->load->view('templates/sidebar', $data);
+        $this->load->view('pages/forms/forms-budget', $data);
+        $this->load->view('templates/footer', $data);
+    }
+
+}
