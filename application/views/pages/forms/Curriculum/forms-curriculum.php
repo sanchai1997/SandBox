059d364ff1ca3bd0dd181d8b7de1afc2c8095add
Nateekarn6215 <Nateekarn.wiangudom1@gmail.com>
2023-03-09 15:06:39 +0700

    update script

--- application/views/pages/forms/Curriculum/forms-curriculum.php
+++ application/views/pages/forms/Curriculum/forms-curriculum.php
@@ -84,7 +84,7 @@
                   <label class="col-sm-2 col-form-label">ประเภทหลักสูตร</label>
                   <div class="col-sm-10">
                     <select class="form-select" aria-label="Default select example" name="CurriculumCode" id="CurriculumCode">
-                      <option selected value="-1">เลือกหลักสูตร</option>
+                      <option selected value="-1">เลือกประเภทหลักสูตร</option>
                       <?php foreach($listCurriculumType as $ls) { ?>
                         <option value="<?php echo $ls->CurriculumCode; ?>"><?php echo $ls->CurriculumTypeName; ?></option>
                       <?php } ?>
@@ -107,8 +107,8 @@
                 <div class="row mb-3">
                   <label class="col-sm-2 col-form-label">ชั้นปี</label>
                   <div class="col-sm-10">
-                    <select class="form-select" aria-label="Default select example" name="GradeLevelCode" id="GradeLevelCode">>
-                      <option selected>เลือกชั้นปี</option>
+                    <select class="form-select" aria-label="Default select example" name="GradeLevelCode" id="GradeLevelCode">
+                      <option selected value="-1">เลือกชั้นปี</option>
                       <?php foreach($listGradeLevel as $ls) { ?>
                         <option value="<?php echo $ls->GradeLevelCode; ?>"><?php echo $ls->GradeLevelName; ?></option>
                       <?php } ?>
@@ -119,14 +119,14 @@
                 <div class="row mb-3">
                   <label for="inputFile" class="col-sm-2 col-form-label">จำนวนห้องที่สอนด้วยหลักสูตร</label>
                   <div class="col-sm-10">
-                    <input type="number" class="form-control"  name="ClassroomNumber" id="ClassroomNumber" min="1" max="999" placeholder="จำนวนห้องที่สอนด้วยหลักสูตร">
+                    <input type="text" class="form-control"  name="ClassroomNumber" id="ClassroomNumber"  placeholder="จำนวนห้องที่สอนด้วยหลักสูตร" maxLength="3">
                   </div>
                 </div>
 
                 <div class="row mb-3">
                   <label for="inputFile" class="col-sm-2 col-form-label">เอกสารหลักสูตร</label>
                   <div class="col-sm-10">
-                    <input type="file" class="form-control"  name="CurriculumDocumentURL" id="CurriculumDocumentURL">
+                    <input type="file" class="form-control"  name="CurriculumDocumentURL1" id="CurriculumDocumentURL1">
                   </div>
                 </div>
 
@@ -159,8 +159,8 @@
 <!-- End Form ข้อมูลหลักสูตร -->
 
               <!-- start Form ข้อมูลหลักสูตรรายวิชา -->
-             
-                <h5 class="card-title">ข้อมูลหลักสูตรรายวิชา   ** สามารถมีได&หลายรายการ และไมfจำเปsนต&องกรอก **</h5>
+             <form name="ttttt" id="tttt">
+                <h5 class="card-title">ข้อมูลหลักสูตรรายวิชา   ** สามารถมีได&หลายรายการ และไม่จำเป็นต&องกรอก **</h5>
 
                 <div class="row mb-3">
                   <label for="inputText" class="col-sm-2 col-form-label">ชื่อรายวิชา</label>
@@ -180,7 +180,7 @@
                   <label class="col-sm-2 col-form-label">กลุ่มสาระการเรียนรู้ / การศึกษาค้นคว้าด้วยตนเอง</label>
                   <div class="col-sm-10">
                     <select class="form-select" aria-label="Default select example" name="SubjectGroupCode" id="SubjectGroupCode">
-                      <option selected value="0">เลือกกลุ่มสาระการเรียนรู้ / การศึกษาค้นคว้าด้วยตนเอง</option>
+                      <option selected value="-1">เลือกกลุ่มสาระการเรียนรู้ / การศึกษาค้นคว้าด้วยตนเอง</option>
                       <?php foreach($listSubjectGroup as $ls) { ?>
                         <option value="<?php echo $ls->SubjectGroupCode; ?>"><?php echo $ls->SubjectGroupName; ?></option>
                       <?php } ?>
@@ -192,7 +192,7 @@
                   <label class="col-sm-2 col-form-label">ประเภทวิชา</label>
                   <div class="col-sm-10">
                     <select class="form-select" aria-label="Default select example" name="SubjectTypeCode" id="SubjectTypeCode">
-                      <option selected value="0">เลือกประเภทวิชา</option>
+                      <option selected value="-1">เลือกประเภทวิชา</option>
                       <?php foreach($listSubjectType as $ls) { ?>
                         <option value="<?php echo $ls->SubjectTypeCode; ?>"><?php echo $ls->SubjectTypeName ; ?></option>
                       <?php } ?>
@@ -210,10 +210,11 @@
                 <div class="row mb-3">
                   <label for="inputText" class="col-sm-2 col-form-label">จำนวนชั่วโมงเรียน</label>
                   <div class="col-sm-10">
-                    <input type="text" class="form-control" name="LearningHour"id="LearningHour">
+                    <input type="text" class="form-control" name="LearningHour"id="LearningHour" placeholder="จำนวนชั่วโมงเรียน">
                   </div>
                 </div>
-
+               
+                    
               <!-- End Form ข้อมูลหลักสูตรรายวิชา -->
 
               <!-- start Form ข้อมูลสมรรถนะของหลักสูตร -->
@@ -223,14 +224,15 @@
                   <label class="col-sm-2 col-form-label">สมรรถนะ</label>
                   <div class="col-sm-10">
                     <select class="form-select" aria-label="Default select example" name="CompetencyCode" id="CompetencyCode">
-                      <option selected value="0">เลือกสมรรถนะ</option>
+                      <option selected value="-1">เลือกสมรรถนะ</option>
                       <?php foreach($listCompetency as $ls) { ?>
                         <option value="<?php echo $ls->CompetencyCode; ?>"><?php echo $ls->CompetencyName; ?></option>
                       <?php } ?> 
                     </select>                   
                   </div>
                 </div>
-
+                <button type="submit" class="btn btn-primary" onclick="return check_2(ttttt)">ยืนยัน2</button>
+                </form>
               <!-- End Form ข้อมูลสมรรถนะของหลักสูตร -->
 
             </div>
@@ -245,7 +247,7 @@
 <script type="text/javascript">
   function check(frm){
   
-  //Check_EducationYear
+  //Check_EducationYear (ปีการศึกษา)
   var EDUCATION = /^[0-9]{4}$/;
     /*
     if(EDUCATION.test(frm.EducationYear.value) == false){
@@ -262,42 +264,76 @@
         frm.EducationYear.value = "";
         return false;
     }
-  //Check_Semester
+
+    //Check_Semester (ภาคเรียน)
     if(frm.Semester.value==-1){
       alert("กรุณาเลือกภาคเรียน");
       return false;
     }
-  /*
-  //Check_CURRICULUM_NAME
+
+    //check_school(สถานศึกษา) 
+    if(frm.SchoolID.value==-1){
+        alert("กรุณาเลือกสถานศึกษา");
+        return false;
+    }
+  
+    //Check_CURRICULUM_NAME(ชื่อหลักสูตร)
     var CHECK_CURRICULUM_NAME = /^[A-Z,a-z,ก-์,0-9]{1,255}$/;
-    if(frm.CURRICULUM_NAME.value==""){
+    if(frm.CurriculumName.value==""){
         alert("กรุณากรอกชื่อหลักสูตร");
-        frm.CURRICULUM_NAME.value = "";
+        frm.CurriculumName.value = "";
         return false;
     }
-    else if(!frm.CURRICULUM_NAME.value.match(CHECK_CURRICULUM_NAME)){
+    else if(!frm.CurriculumName.value.match(CHECK_CURRICULUM_NAME)){
         alert("กรุณากรอกชื่อหลักสูตรให้ถูกต้อง");
-        frm.CURRICULUM_NAME.value = "";
+        frm.CurriculumName.value = "";
         return false;
     }
-  */
-    //Check_CurriculumCode
+  
+    //Check_CurriculumCode(ประเภทหลักสูตร)
     if(frm.CurriculumCode.value==-1){
-      alert("กรุณาเลือกรหัสหลักสูตร");
+      alert("กรุณาเลือกประเภทหลักสูตร");
       return false;
     }
-    //Check_EducationLevelCode
+
+    //Check_EducationLevelCode(ระดับการศึกษา)
     if(frm.EducationLevelCode.value==-1){
       alert("กรุณาเลือกรหัสระดับการศึกษา");
       return false;
     }
-    //Check_LocalCurriculumFlag
+
+    //Check_GradeLevelCode (ชั้นปี)
+    if(frm.GradeLevelCode.value==-1){
+      alert("กรุณาเลือกชั้นปี");
+      return false;
+    }
+
+    //Check_ClassroomNumber(จำนวนห้องที่สอนด้วยหลักสูตร)
+    var check_ClassroomNumber = /^[0-9]{1,3}$/;
+    if(frm.ClassroomNumber.value ==""){
+        alert("กรุณากรอกข้อมูลจำนวนห้องที่สอนด้วยหลักสูตร");
+        frm.ClassroomNumber.value = "";
+        return false;
+    }else if (!frm.ClassroomNumber.value.match(check_ClassroomNumber)){
+        alert("กรุณากรอกข้อมูลจำนวนห้องที่สอนด้วยหลักสูตรตัวเลขเท่านั้น");
+        frm.ClassroomNumber.value = "";
+        return false;
+    }
+    
+    //Check_CurriculumDocumentURL1(เอกสารหลักสูตร)
+    if(frm.CurriculumDocumentURL1.value==""){
+        alert("กรุณาแนบไฟล์เอกสารหลักสูตร");
+        return false;
+    }
+    
+    //Check_LocalCurriculumFlag(ชื่อหลักสูตรท้องถิ่นที่อ้างอิง)
     if(frm.LocalCurriculumFlag.value==-1){
       alert("กรุณาเลือกอ้างอิงหลักสูตรท้องถิ่น");
       return false;
     }
-   //Check_LocalCurriculumName
-   var CHECK_LOCAL_CURRICULUM_NAME = /^[A-Z,a-z,ก-์,0-9]{1,255}$/;
+
+    //Check_LocalCurriculumName(ชื่อหลักสูตรท้องถิ่นที่อ้างอิง)
+    var CHECK_LOCAL_CURRICULUM_NAME = /^[A-Z,a-z,ก-์,0-9]{1,255}$/;
     if(frm.LocalCurriculumName.value==""){
         alert("กรุณากรอกชื่อหลักสูตรท้องถิ่นที่อ้างอิง");
         frm.LocalCurriculumName.value = "";
@@ -309,10 +345,53 @@
         return false;
     }
 
+    //Check_LocalCurriculumDocumentURL(เอกสารแนบหลักสูตรอ้างอิง)
+    if(frm.LocalCurriculumDocumentURL.value==""){
+        alert("กรุณาแนบไฟล์เอกสารแนบหลักสูตรอ้างอิง");
+        return false;
+    }
+  }
+  function check_2(frm2){
+    
+    //Check_CHECK_SubjectName(ชื่อรายวิชา)
+    var CHECK_SubjectName = /^[A-Z,a-z,ก-์,0-9]{1,100}$/;
+    if(frm2.SubjectName.value==""){
+        alert("กรุณากรอกชื่อรายวิชา");
+        return false;
+    }else if(!frm2.SubjectName.value.match(CHECK_SubjectName)){
+      alert("กรุณากรอกชื่อรายวิชาให้ถูกต้อง");
+      return false;
+    }
+    //Check_CHECK_SubjectCode(รหัสวิชา)
+    var CHECK_SubjectCode = /^[A-Z,a-z,ก-์,0-9]{1,10}$/;
+    if(frm2.SubjectCode.value==""){
+        alert("กรุณากรอกรหัสวิชา");
+        return false;
+    }else if(!frm2.SubjectCode.value.match(CHECK_SubjectCode)){
+      alert("กรุณากรอกรหัสวิชาให้ถูกต้อง");
+      return false;
+    }
+    //Check_SubjectGroupCode(กลุ่มสาระการเรียนรู้ / การศึกษาค้นคว้าด้วยตนเอง)
+    if(frm2.SubjectGroupCode.value==-1){
+      alert("กรุณาเลือกกลุ่มสาระการเรียนรู้ / การศึกษาค้นคว้าด้วยตนเอง");
+      return false;
+    }
+     //Check_SubjectTypeCode(รหัสประเภทรายวิชา)
+     if(frm2.SubjectTypeCode.value==-1){
+      alert("กรุณาเลือกประเภทรายวิชา");
+      return false;
+    }
+     //Check_CompetencyCode(สมรรถนะ)
+     if(frm2.CompetencyCode.value==-1){
+      alert("กรุณาเลือกสมรรถนะ");
+      return false;
+    }
 
+    
+
+    
 
     
   }
 </script>    
-
-  </main><!-- End #main -->
+</main><!-- End #main -->
