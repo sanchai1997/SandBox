0956f18a940b811b1d3cf6cbcee253712f2a7f32
sanchai1997 <sanyakorn.Chai@gmail.com>
2023-03-14 16:34:50 +0700

    Revert "update DateThai"
    This reverts commit b63c23b03086d6b2ce5d8d686f0455ddc6fdbf63.

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -268,23 +268,3 @@ defined('BASEPATH') or exit('No direct script access allowed');
     </nav><!-- End Icons Navigation -->
 
   </header><!-- End Header -->
-
-  <!------------------------ format Thai -----------------------!>
-    <?php
-    function DateThai($strDate)
-    {
-      $strYear = date("Y", strtotime($strDate)) + 543;
-      $strMonth = date("n", strtotime($strDate));
-      $strDay = date("j", strtotime($strDate));
-      $strHour = date("H", strtotime($strDate));
-      $strMinute = date("i", strtotime($strDate));
-      $strSeconds = date("s", strtotime($strDate));
-      $strMonthCut = array("", "มกราคม", "กุมภาพันธ์", "มีนาคม", "เมษายน", "พฤษภาคม", "มิถุนายน", "กรกฎาคม", "สิงหาคม", "กันยายน", "ตุลาคม", "พฤศจิกายน", "ธันวาคม");
-      $strMonthThai = $strMonthCut[$strMonth];
-      return "$strDay $strMonthThai $strYear";
-    }
-
-    $strDate = "2008-08-14 13:42:44";
-    echo "ThaiCreate.Com Time now : " . DateThai($strDate);
-    ?>
-  <!-------------------- format Thai - END ---------------------!>
