6b11bafc8b1d9f6d025ac99bc67334648bc26130
wanwisa-6222 <107360342+fanggffff@users.noreply.github.com>
2023-03-31 01:27:46 +0700

    edit & fix bug any form

--- application/views/pages/dashboard/Curriculum/list-curriculum_activity.php
+++ application/views/pages/dashboard/Curriculum/list-curriculum_activity.php
@@ -56,17 +56,82 @@
                                 <th style="text-align: center;" scope="col"><?php echo$lca->ACTIVITY_NAME ?></th>
                                 
                                 <td style="text-align: center;">
-                                       
-                                        <a href='edit_forms-curriculum_activity?pid=<?php echo$lca->PLAN_ID ?>&&ACTIVITY_ID=<?php echo$lca->ACTIVITY_ID ?>&&sid=<?php echo $SubjectCode; ?>&&cid=<?php echo $CurriculumID; ?>' class="btn btn-warning">
-                                            <i class="bi bi-pencil-square"></i>
-                                        </a>
-                                        <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#delete">
-                                            <i class=" bi bi-trash"></i>
-                                        </button>
+                                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#view<?php echo $lca->ACTIVITY_ID; ?><?php echo $lca->PLAN_ID; ?>"><i class="bi bi-card-list"></i></button>
+                                    <a href='edit_forms-curriculum_activity?pid=<?php echo$lca->PLAN_ID ?>&&ACTIVITY_ID=<?php echo$lca->ACTIVITY_ID ?>&&sid=<?php echo $SubjectCode; ?>&&cid=<?php echo $CurriculumID; ?>' class="btn btn-warning">
+                                        <i class="bi bi-pencil-square"></i>
+                                    </a>
+                                    <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#delete<?php echo $lca->ACTIVITY_ID; ?><?php echo $lca->PLAN_ID; ?>">
+                                        <i class=" bi bi-trash"></i>
+                                    </button>
                                 </td>
                             </tr>
+                            <!-- Modal view -->
+                            <div class="modal fade" id="view<?php echo $lca->ACTIVITY_ID; ?><?php echo $lca->PLAN_ID; ?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable modal-xl">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title" id="exampleModalLabel" style="padding-left: 30px; padding-top: 15px;"> <i class="bi bi-card-heading"></i>ข้อมูลกิจกรรม </h5>
+                                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                            <hr>
+                                        </div>
+                                        <div class="modal-body" style="padding-left: 70px; padding-top: 20px;">
+                                            <div class="row">
+                                                <h6 style="padding-top: 10px;"><b>กิจกรรม</b></h6>
+                                                    <div class=" col-8" style="padding-bottom: 8px; padding-left: 40px;">
+                                                        ชื่อกิจกรรม : <?php echo $lca->ACTIVITY_NAME  ?>
+                                                    </div>
+                                            </div>
+                                            <div class="row">
+                                                    <h6 style="padding-top: 10px;"><b>การวัดและประเมินผล</b></h6>
+                                                    <div class=" col-8" style="padding-bottom: 8px; padding-left: 40px;">
+                                                        ชื่อการวัดและประเมินผล : <?= $lca->ASSESSMENT_NAME; ?>
+                                                    </div>
+                                                    <div class=" col-8" style="padding-bottom: 8px; padding-left: 40px;">
+                                                        รหัสผู้ประเมิน : <?php $PEOPLE_code = $lca->ASSESSMENT_PEOPLE_ID;
+                                                                    if ($PEOPLE_code == 0) echo "ครู";
+                                                                    else if ($PEOPLE_code == 1) echo "นักเรียน";
+                                                                    else if ($PEOPLE_code == 2) echo "ผู้ปกครอง";
+                                                                    else if ($PEOPLE_code == 3) echo "อื่นๆ"; ?>
+                                                    </div>
+                                                    <div class=" col-8" style="padding-bottom: 8px; padding-left: 40px;">
+                                                        เครื่องมือการประเมิน :  <?php 
+                                                                    if ($lca->ASSESSMENT_TOOL_CODE == 0) echo "แบบสังเกตพฤติกรรม";
+                                                                    else  echo "แบบประเมิน";?>
+                                                    </div>
+
+                                            </div>
+                                            <div class="row">
+                                                    <h6 style="padding-top: 10px;"><b>การวัดผลมาตรฐานกลาง</b></h6>
+                                                    <div class=" col-8" style="padding-bottom: 8px; padding-left: 40px;">
+                                                        สัดส่วนการประเมินโดยผู้สอน : <?= $lca->SCORE_TEACHER; ?>
+                                                    </div>
+                                                    <div class=" col-8" style="padding-bottom: 8px; padding-left: 40px;">
+                                                        สัดส่วนการประเมินโดยผู้ปกครอง : <?= $lca->SCORE_PARENT; ?>
+                                                    </div>
+                                                    <div class=" col-8" style="padding-bottom: 8px; padding-left: 40px;">
+                                                        สัดส่วนการประเมินโดยบุคลากรอื่น : <?= $lca->SCORE_OTHER; ?>
+                                                    </div>
+                                                    <div class=" col-8" style="padding-bottom: 8px; padding-left: 40px;">
+                                                        คะแนนรวม : <?= $lca->SCORE_SUM_TOTAL; ?>
+                                                    </div>
+                                                    <div class=" col-8" style="padding-bottom: 8px; padding-left: 40px;">
+                                                        ผลการตัดสินรายวิชาพื้นฐาน : <?php $PEOPLE_code = $lca->FUNDAMENTAL_SUBJECT_PASSING_NAME;?>
+                                                    </div>
+            
+
+                                            </div>
+                                    </div>
+                                        <div class="modal-footer">
+                                        <a href='edit_forms-curriculum_activity?pid=<?php echo$lca->PLAN_ID ?>&&ACTIVITY_ID=<?php echo$lca->ACTIVITY_ID ?>&&sid=<?php echo $SubjectCode; ?>&&cid=<?php echo $CurriculumID; ?>' class="btn btn-warning"><i class="bi bi-pencil-square"></i></a>
+                                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">ปิด</button>                                            </div>
+                                        </div>
+                                </div>
+                            </div>
+                            <!----------------------------  END Modal view --------------------------------->
+
+
                           <!-- Modal delete -->
-                          <div class="modal fade" id="delete" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+                          <div class="modal fade" id="delete<?php echo $lca->ACTIVITY_ID; ?><?php echo $lca->PLAN_ID; ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
                                     <div class="modal-dialog modal-dialog-centered" role="document">
                                         <div class="modal-content">
                                             <div class="modal-header">
