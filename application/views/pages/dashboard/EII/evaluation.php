5b6946da5dc8722b1a9bc35b6c3fddf339b565fa
Sophon09511 <116242625+Sophon09511@users.noreply.github.com>
2023-04-13 22:05:52 +0700

    hide_Description

--- application/views/pages/dashboard/EII/evaluation.php
+++ application/views/pages/dashboard/EII/evaluation.php
@@ -1436,16 +1436,16 @@
                             </table>
                         </td>
                     </tr>
-                    <tr>
+                    <!-- <tr>
                         <td><a href="<?php echo site_url('ass_ria_com_lvl_forms_p4?page=sh4') ?>"
                                 class="my-link fw-bold">>>เพิ่มคำอธิบาย>>
                             </a></td>
-                    </tr>
-                    <tr>
+                    </tr> -->
+                    <!-- <tr>
                         <th>คำอธิบายตัวชี้วัด</th>
                         <th>คำอธิบายองค์ประกอบของตัวชี้วัด </th>
-                    </tr>
-                    <tr>
+                    </tr> -->
+                    <!-- <tr>
                         <td>
                             <table class="table table-bordered">
                                 <thead class="table-Ligth">
@@ -1506,7 +1506,7 @@
                                 <?php } ?>
                             </table>
                         </td>
-                    </tr>
+                    </tr> -->
                 </table>
 
             </div>
